f3451ce4bbb3686fe9db111026c85808e25ed151
7Nikita <nikita.mda@yandex.by>
2020-09-02 18:38:02 +0300

    Initial Commit

---
+++ Toaster/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Toaster
+//
+//  Created by Nikita Pekurin on 9/2/20.
+//  Copyright © 2020 Nikita Pekurin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
