cc408ddca8b08cc809bb4bc292c91ca152ea6797
7Nikita <nikita.mda@yandex.by>
2020-09-12 20:27:02 +0300

    added toast codable model

---
+++ Toaster/Models/Toast.swift
@@ -0,0 +1,13 @@
+//
+//  Toast.swift
+//  Toaster
+//
+//  Created by Nikita Pekurin on 9/12/20.
+//  Copyright © 2020 Nikita Pekurin. All rights reserved.
+//
+
+import Foundation
+
+struct Toast: Codable {
+    let content: String
+}
---
+++ Toaster/Views/ToastTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ToastTableViewCell.swift
+//  Toaster
+//
+//  Created by Nikita Pekurin on 9/12/20.
+//  Copyright © 2020 Nikita Pekurin. All rights reserved.
+//
+
+import UIKit
+
+class ToastTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
